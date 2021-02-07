dbe25d2eb4d0de4f7d0d53a38ac32e7231f9384b
black-mesa-canteen <shaotienlee@foxmail.com>
2021-02-07 16:04:43 +0800

    finished categories

--- src/main/java/com/learn/blog_demo/web/admin/CategoryController.java
+++ src/main/java/com/learn/blog_demo/web/admin/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -63,9 +64,43 @@ public class CategoryController {
         if(type1 == null) {
             attributes.addFlashAttribute("message", "Operation Failed");
         } else {
-            attributes.addFlashAttribute("message", "Successed!");
+            attributes.addFlashAttribute("message", "Success!");
         }
 
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/categories/{id}/input")
+    public String editInput(@PathVariable Long id, Model model) {
+        model.addAttribute("type", categoryService.getType(id));
+        return "admin/category-input";
+    }
+
+    @PostMapping("/categories/{id}")
+    public String editPost(@Valid Type type, BindingResult result, @PathVariable Long id, RedirectAttributes attributes) {
+        Type t = categoryService.getTypeByName(type.getName());
+        if(t != null) {
+            result.rejectValue("name", "nameError", "No Duplicates!");
+            t = null;
+        }
+
+        if(result.hasErrors()) {
+            return "admin/category-input";
+        }
+        Type type1 = categoryService.updateType(id, type);
+        if(type1 == null) {
+            attributes.addFlashAttribute("message", "Updating Failed");
+        } else {
+            attributes.addFlashAttribute("message", "Success!");
+        }
+
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("categories/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
+        categoryService.deleteType(id);
+        attributes.addFlashAttribute("message", "Deleted!");
         return "redirect:/admin/categories";
     }
 
